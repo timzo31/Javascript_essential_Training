5ddd9b25b04f7c00f24504a91b9ff73a32890472
timzo31 <madty31@gmail.com>
2022-08-18 19:20:40 +0800

    Variables: block scope vaiables Let & const, Done

--- 6_Variables&Types/6_03/script.js
+++ 6_Variables&Types/6_03/script.js
@@ -3,24 +3,32 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
 
-var color = "purple";
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
 color = "skyblue";
 
-function headingColor() {
-  color = "blue";
-  document.querySelector(".title").style.color = color;
-}
-
+// Global scope issue
+// function headingColor() {
+//   color = "blue";
+//   document.querySelector(".title").style.color = color;
+// }
 //headingColor();
+
+// Block-scope solution
+function headingColor() {
+  let titleColor = "blue";
+  document.querySelector(".title").style.color = color;
+  console.log("inside: ", titleColor);
+}
+headingColor();
+
+console.log("outside: ", titleColor);
 
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
-
-headingColor();
 
 // function headingColor() {
 //   color = "blue";
