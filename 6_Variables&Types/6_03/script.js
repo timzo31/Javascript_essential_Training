7533a44cb7d26755dec34db71a2ac798833f750e
timzo31 <madty31@gmail.com>
2022-08-18 19:11:48 +0800

    Variables: Var and the Global Scope issues, Done

---
+++ 6_Variables&Types/6_03/script.js
@@ -0,0 +1,30 @@
+/**
+ * Scope and the var statement
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+ */
+
+var color = "purple";
+
+document.querySelector(".left").style.backgroundColor = color;
+document.querySelector(".left .color-value").innerHTML = color;
+
+color = "skyblue";
+
+function headingColor() {
+  color = "blue";
+  document.querySelector(".title").style.color = color;
+}
+
+//headingColor();
+
+document.querySelector(".right").style.backgroundColor = color;
+document.querySelector(".right .color-value").innerHTML = color;
+
+headingColor();
+
+// function headingColor() {
+//   color = "blue";
+//   document.querySelector(".title").style.color = color;
+// }
+
+// headingColor();
